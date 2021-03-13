e3b37ab9f88087d7e4d364aff6ffdd7f4cdb4a9a
flymyart <flymyart@yandex.ru>
2021-03-13 18:24:43 +0300

    2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,20 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        boolean comparing = bean == bean1;
+        System.out.println(bean);
+        System.out.println(bean1);
+        System.out.println(comparing);
+
+        Cat beanCat =
+                (Cat) applicationContext.getBean("cat");
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+
+        boolean comparing1 = beanCat == beanCat1;
+        System.out.println(beanCat);
+        System.out.println(beanCat1);
+        System.out.println(comparing1);
     }
 }
